876fedb49cb9a2489aad1e226189dba394a2c78e
msivakumar7 <msivakumarofficial76>
2024-12-24 14:50:50 +0530

    Uploaded Advance_js(Tasks 11-35)

---
+++ Task28.js
@@ -0,0 +1,6 @@
+export function namedExport(){
+    return "This is the javascript file,It contains Named export are here!!!";
+}
+export function namedExport2(){
+    return "This is the another javascript file for named export!!!";
+}
